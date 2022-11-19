f04a3eb9cc32db54cfd87ceecdc8cfc081af204c
ZuzaBedkowska <zbedkowska@student.agh.edu.pl>
2022-11-19 16:10:26 +0100

    Kula lata

---
+++ Gorlami/Assets/Schooting.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Schooting : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb.useGravity = false; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 20);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //print("collision!\n");
+    }
+}
